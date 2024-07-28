57c0134d2778fcb7242faad5609e837d159c26f4
DJCreeper <167189379+DJCreeperYTB@users.noreply.github.com>
2024-07-28 15:15:39 +0200

    sa

---
+++ scripts.js
@@ -0,0 +1,14 @@
+// scripts.js
+
+function loadContent(page) {
+    fetch(page)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('content').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading page:', error));
+}
+
+function openVideo(url) {
+    window.location.href = url;
+}
